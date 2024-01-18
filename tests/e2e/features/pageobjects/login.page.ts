f0a3a1fe23bab90358c637208aa1c18a42ee6458
ola-fawole85 <154567741+ola-fawole85@users.noreply.github.com>
2024-01-18 14:20:51 +0000

    Update login.page.ts

--- tests/e2e/features/pageobjects/login.page.ts
+++ tests/e2e/features/pageobjects/login.page.ts
@@ -20,6 +20,10 @@ class LoginPage extends Page {
         return $('button[type="submit"]');
     }
 
+    public get arrayOfStocks() {
+		return $$('#arrayOfStocks');
+	}
+
     /**
      * a method to encapsule automation code to interact with the page
      * e.g. to login using username and password
@@ -29,6 +33,24 @@ class LoginPage extends Page {
         await this.inputPassword.setValue(password);
         await this.btnSubmit.click();
     }
+
+    public async selectStocks(companyName: string) {
+		const stocks = await this.arrayOfStocks;
+		let found = false;
+
+		for (const stock of stocks) {
+			const stockName = await stock.getText();
+			if (stockName === companyName) {
+				await stock.click();
+				found = true;
+				break;
+			}
+		}
+
+		if (!found) {
+			throw new Error(`Text "${companyName}" not found in the search fields.`);
+		}
+	}
 
     /**
      * overwrite specific options to adapt it to page object
